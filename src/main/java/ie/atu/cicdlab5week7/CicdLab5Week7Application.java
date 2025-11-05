fad5633c519a2bb023f09411174019292fd5dcf9
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-11-05 14:41:58 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/cicdlab5week7/CicdLab5Week7Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlab5week7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLab5Week7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLab5Week7Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicdlab5week7/CicdLab5Week7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlab5week7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLab5Week7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
