5ab318befb01055d76eb47d7ec8fdd35a0ba3b29
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-11-12 16:06:33 +0000

    In Docker api not running db okay

--- src/main/java/ie/atu/cicdlab5week7/CicdLab6Application.java
+++ src/main/java/ie/atu/cicdlab5week7/CicdLab6Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdLab5Week7Application {
+public class CicdLab6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CicdLab5Week7Application.class, args);
+        SpringApplication.run(CicdLab6Application.class, args);
     }
 
 }
